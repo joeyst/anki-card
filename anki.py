af332bf011ae165adc7e4be6f5f58d55f600692b
joeyst <joeystenbeck@gmail.com>
2024-01-25 17:24:19 -0800

    Add setup.py, .gitignore.

---
+++ anki.py
@@ -0,0 +1,2 @@
+
+from anki_card import AnkiCard as Card
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+  name="anki",
+  version="0.1",
+  author="Joey Stenbeck",
+  py_modules=["anki"],
+)
